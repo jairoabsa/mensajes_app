9b7b21ef2cdda44ff20f3dc575e3124f3dbc6ad4
Jairo Espinoza <jairoabsa@gmail.com>
2023-06-04 22:50:44 -0600

    agregar la creacion de un mensaje y en el menu inicio la coneccion con el metodo de MensajesServices.crearMensajes para iniciar el proceso de creacion ademas conectarlo con MensajesDAO el metodo crearMensajesDB y agregar los registro por medio del get y set.

--- src/main/java/MensajesDAO.java
+++ src/main/java/MensajesDAO.java
@@ -1,6 +1,26 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MensajesDAO {
     public static void crearMensajeBD(Mensajes mensaje){
+        Conexion db_connect = new Conexion();
 
+        try(Connection conexion = db_connect.get_connection()){
+            PreparedStatement ps=null;
+            try{
+                String query = "INSERT INTO mensajes (mensaje, autor_mensaje) VALUES (?, ?)";
+                ps=conexion.prepareStatement(query);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setString(2, mensaje.getAutor_mensaje());
+                ps.executeUpdate();
+                System.out.println("Mensaje Creado");
+            }catch(SQLException ex){
+                System.out.println(ex);
+            }
+        }catch(SQLException e){
+            System.out.println(e);
+        }
     }
     public static void leerMensajesDB(){
 
--- src/main/java/MensajesServices.java
+++ src/main/java/MensajesServices.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class MensajesServices {
     public static void crearMensajes(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu mensaje");
+        String mensaje = sc.nextLine();
 
+        System.out.println("Tu nombre");
+        String nombre = sc.nextLine();
+
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutor_mensaje(nombre);
+        MensajesDAO.crearMensajeBD(registro);
     }
     public static void listarMensajes(){
 
